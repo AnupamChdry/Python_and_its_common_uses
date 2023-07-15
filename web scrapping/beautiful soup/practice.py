ba5f64e28b241c66027226a75a3712ecc835662c
anupa <anupam19092003@gmail.com>
2023-07-16 04:22:13 +0530

    MAde some Changes

--- web scrapping/beautiful soup/practice.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-url="https://codewithharry.com"
-r = requests.get(url)
-htmlcontent = r.content
-soup = BeautifulSoup(htmlcontent, 'html.parser')
-# print(soup.prettify)
-title=soup.title
-print(title)
